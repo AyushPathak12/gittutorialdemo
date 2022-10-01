7f2351f30a5cb16fd20a3ddc832f50a81cac2bf6
AyushPathak13 <72655042+AyushPathak13@users.noreply.github.com>
2022-10-01 10:45:37 +0530

    Add files via upload
    Added your name

---
+++ static/ur name.py
@@ -0,0 +1,7 @@
+import turtle
+
+turtle.bgcolor('black')
+turtle.color('Red')
+style = ('Courier', 30, 'bold', 'italic')
+turtle.write('***AYUSH PATHAK***', font=style, align='center')
+turtle.hideturtle()
